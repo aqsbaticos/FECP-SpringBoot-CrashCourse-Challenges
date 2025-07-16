fb2f077ed9b6919c24f565d630550a7eb00a73d6
Andric Quinn Baticos <andric.baticos@gcash.com>
2025-07-16 15:41:00 +0800

    split src code to different challenges

---
+++ challenge1/controller/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.example.demo.myfirstapi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+
+	@Value("${app.greeting.message}")
+	private String customGreeting;
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return customGreeting;
+	}
+
+	@GetMapping("/greet/{name}")
+	public String greetUser(@PathVariable String name) {
+		return "Greetings, " + name + "!";
+	}
+
+}
