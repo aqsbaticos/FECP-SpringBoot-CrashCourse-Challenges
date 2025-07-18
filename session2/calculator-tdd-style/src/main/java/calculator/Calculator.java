58e3ec9cca991477fdbb818d74aa7a48fc9e6f0f
Frondozo, Baticos <ivan+andric@gcash.com>
2025-07-18 11:51:08 +0800

    add unit test: difference, product

--- session2/calculator-tdd-style/src/main/java/calculator/Calculator.java
+++ session2/calculator-tdd-style/src/main/java/calculator/Calculator.java
@@ -2,4 +2,16 @@ package calculator;
 
 public class Calculator {
 
+	public double sum(double a, double b) {
+		return a+b;
+	}
+
+	public double difference(double a, double b) {
+		return a-b;
+	}
+
+	public double product(double a, double b) {
+		return a*b;
+	}
+
 }
--- session2/calculator-tdd-style/src/test/java/calculator/CalculatorTest.java
+++ session2/calculator-tdd-style/src/test/java/calculator/CalculatorTest.java
@@ -14,10 +14,23 @@ class CalculatorTest {
 		calculator = new Calculator();
 	}
 
-
 	@Test
 	void shouldReturnCorrectSum() {
 		double result = calculator.sum(1,2);
 		assertEquals(3, result, 0.01);
+	}
+
+	@Test
+	void shouldReturnCorrectDifference() {
+		double a = 1, b = 2, expected = -1;
+		double actual = calculator.difference(a,b);
+		assertEquals(expected, actual, 0.01);
+	}
+
+	@Test
+	void shouldReturnCorrectProduct() {
+		double a = 1, b = 2, expected = 2;
+		double result = calculator.product(a,b);
+		assertEquals(2, result, 0.01);
 	}
 }
